42e0a5493f69858da885293db10f2f4cdf702333
qwertymodo <qwertymodo@qwertymodo.com>
2017-03-25 19:01:01 -0700

    Fix calculating loop point with start padding

--- msupcm++/AudioSubTrack.cpp
+++ msupcm++/AudioSubTrack.cpp
@@ -167,7 +167,7 @@ void AudioSubTrack::render()
 			sox->fade(m_fade_in, m_fade_out);
 			sox->pad(m_pad_start, m_pad_end);
 			sox->tempo(m_tempo);
-			sox->loop(m_trim_start + m_start_offset, m_loop);
+			sox->loop(m_trim_start + m_start_offset, m_loop + m_pad_start);
 			sox->dither(m_dither_type);
 			sox->finalize();
 		}
